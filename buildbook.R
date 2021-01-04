cf7b98d11d52b46f24e45c9cecb1772fc755113c
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2021-01-04 18:31:19 -0300

    corrects links for edit and view

--- buildbook.R
+++ buildbook.R
@@ -1,13 +1,11 @@
 # Prepare working directory
 renv::init()
-install.packages(c("languageserver", "servr", "lintr", "pander", "DiagrammeR"))
+install.packages(c("languageserver", "servr", "lintr", "pander"))
 renv::snapshot()
 
 # Build and serve book
 bookdown::render_book("index.Rmd")
 bookdown::serve_book()
-# rmarkdown::render_site(encoding = 'UTF-8')
-
 
 # Move files to website repository
 dir <- "~/projects/web/pedometria.org/static/livros/manual-de-gestao-de-dados-de-recursos-do-solo"
