4bec66b5aab913b3ae09fc5c560a93d7a35865bc
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2021-01-04 23:34:32 -0300

    describes Tabula for data extracion

---
+++ resgate-de-dados-do-solo.R
@@ -0,0 +1,8 @@
+## ---- tabula-selecao-dos-dados ----
+knitr::include_graphics("img/tabula-selecao-dos-dados.png")
+
+## ---- tabula-visualizacao-dos-dados-extraidos ----
+knitr::include_graphics("img/tabula-visualizacao-dos-dados-extraidos.png")
+
+## ---- tabula-inconsistencia-nos-dados ----
+knitr::include_graphics("img/tabula-inconsistencia-nos-dados.png")
